c4b30a6630b3ee647b33b06dabdc89b3b996a42e
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-29 08:35:59 -0500

    Add logic for it to name map

--- src/main/java/com/zipcodewilmington/streams/anthropoid/PersonWarehouse.java
+++ src/main/java/com/zipcodewilmington/streams/anthropoid/PersonWarehouse.java
@@ -4,10 +4,7 @@ import com.zipcodewilmington.streams.tools.ReflectionUtils;
 import com.zipcodewilmington.streams.tools.logging.LoggerHandler;
 import com.zipcodewilmington.streams.tools.logging.LoggerWarehouse;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -34,7 +31,7 @@ public final class PersonWarehouse implements Iterable<Person> {
 
     /**
      * @return list of names of Person objects
-     */ // TODO
+     */
     public List<String> getNames() {
         List<String> names = new ArrayList<>();
 
@@ -48,7 +45,13 @@ public final class PersonWarehouse implements Iterable<Person> {
      * @return list of uniquely named Person objects
      */ //TODO
     public Stream<Person> getUniquelyNamedPeople() {
-        return null;
+        List<Person> uniquelyNamedPeople = new ArrayList<>();
+
+        uniquelyNamedPeople = people.stream()
+                .distinct()
+                .collect(Collectors.toList());
+
+        return uniquelyNamedPeople.stream();
     }
 
 
@@ -70,9 +73,14 @@ public final class PersonWarehouse implements Iterable<Person> {
 
     /**
      * @return a mapping of Person Id to the respective Person name
-     */ // TODO
+     */
     public Map<Long, String> getIdToNameMap() {
-        return null;
+        Map<Long,String> idToNameMap = new HashMap<>();
+
+        idToNameMap = people.stream()
+                .collect(Collectors.toMap(Person::getPersonalId, Person::getName));
+
+        return idToNameMap;
     }
 
 
