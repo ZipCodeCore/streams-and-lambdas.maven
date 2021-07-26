e5937fd1b0c713822e176191952ae652a03ed0e5
Dee <dee@zipcodes-MacBook-Pro-2.local>
2021-07-26 14:22:47 -0400

    person streams in factory

--- src/main/java/com/zipcodewilmington/streams/anthropoid/PersonFactory.java
+++ src/main/java/com/zipcodewilmington/streams/anthropoid/PersonFactory.java
@@ -3,6 +3,7 @@ package com.zipcodewilmington.streams.anthropoid;
 import com.zipcodewilmington.streams.tools.RandomUtils;
 import com.zipcodewilmington.streams.tools.StringUtils;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -39,7 +40,12 @@ public final class PersonFactory {
      * @return - ArrayList of Person objects
      */ // TODO
     public List<Person> createPersonList(int listSize) {
-        return null;
+        List<Person> personList = Stream
+                .generate(this :: createRandomPerson)
+                .limit(listSize)
+                .collect(Collectors.toList());
+
+        return personList;
     }
 
 
@@ -48,7 +54,12 @@ public final class PersonFactory {
      * @return - Array of Person objects
      */ // TODO
     public Person[] createPersonArray(int arrayLength) {
-        return null;
+        Person[] personArray =
+                Stream
+                .generate(this::createRandomPerson)
+                .limit(arrayLength)
+                .toArray(Person[]::new);
+        return personArray;
     }
 
 
@@ -59,6 +70,9 @@ public final class PersonFactory {
      * @return - Stream representation of collection of Person objects
      */ // TODO
     public Stream<Person> createPersonStream(int streamCount) {
-        return null;
+        Stream<Person> pStream = Stream
+                .generate(this::createRandomPerson)
+                .limit(streamCount);
+        return pStream;
     }
 }
