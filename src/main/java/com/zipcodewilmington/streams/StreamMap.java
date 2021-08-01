a8a0bab5f90b0ce98146b85ba02deefdff451f2e
laura <laurab90>
2021-08-01 16:05:24 -0400

    done

--- src/main/java/com/zipcodewilmington/streams/StreamMap.java
+++ src/main/java/com/zipcodewilmington/streams/StreamMap.java
@@ -18,7 +18,10 @@ public class StreamMap {
      * @return - a Stream of single characters
      */ //TODO
     public static Stream<String> letters(String someWord) {
-        return null;
+        return Arrays.stream(someWord.split(""));
+//         String[] characters = someWord.split("");
+//
+//        return Stream.of(characters);
     }
 
     /**
@@ -26,7 +29,8 @@ public class StreamMap {
      * @return - a Stream of several Streams of single characters
      */ //TODO
     public static Stream<Stream<String>> wordsMap(String... someWords) {
-        return null;
+        return Arrays.stream(someWords).map(StreamMap::letters);
+//        return Stream.of(someWords).map(w -> letters(w));
     }
 
     /**
@@ -34,6 +38,9 @@ public class StreamMap {
      * @return - a Stream of several Streams of single characters
      */ //TODO
     public static Stream<String> wordsFlatMap(String... stringArray) {
-        return null;
+        return Arrays.stream(stringArray).flatMap(StreamMap::letters);
+
+//        return Stream.of(stringArray).sorted().flatMap((w -> letters(w)));
+
     }
 }
